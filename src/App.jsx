60d757c29b0e647e6434d55405a2440717006dc5
victoriaag6 <victoriaacostag6@gmail.com>
2025-07-02 14:18:43 -0400

    agregando files

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
-  )
+return (
+  <div className="app-welcome">
+    <h1>Bienvenido al Sistema de Soporte Técnico</h1>
+    <p>Comienza a desarrollar tu aplicación aquí.</p>
+  </div>
+)
 }
 
 export default App
